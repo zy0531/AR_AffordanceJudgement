c13d3ed7ed4fefe7a205bfa64550630703b7c349
Carlossr404 <47153697+Carlossr404@users.noreply.github.com>
2020-02-17 14:38:35 -0600

    version1

---
+++ AJ20200213/Assets/DiscCompare.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; // Required when Using UI elements.
+using UnityEngine;
+
+public class DiscCompare : MonoBehaviour
+{
+    //public GUIStyle styleButton;
+    //public GUIStyle styleLabel;
+    public GameObject Cueprefab;
+    public Text DisPoles;
+    public Text ShoulderWidth;
+    public InputField inputfield;
+    public Text success;
+    public Text fail;
+
+    private float timeShown; // results show 2s then disappear
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Distance between 2 Poles
+        float dis = Vector3.Distance(GameObject.Find("PoleRight").transform.position, GameObject.Find("PoleLeft").transform.position) - GameObject.Find("PoleRight").transform.localScale.x;
+        DisPoles.text = "Pole Gap Width:" + dis.ToString();
+        // Shoulder width of participant
+        float inputF = float.Parse(inputfield.text, System.Globalization.CultureInfo.InvariantCulture);
+        ShoulderWidth.text = "Shoulder Width:" + inputfield.text;
+
+        // compare dis
+        GameObject.Find("Go").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            timeShown = 0.0f;
+            if (dis <= inputF)
+            {
+                fail.text = "Poles fell down!";
+                success.text = "";
+            }
+            else
+            {
+                success.text = "You went through successfully!";
+                fail.text = "";
+            }
+        }
+        );
+        // show 2s and then disappear
+        timeShown += Time.deltaTime;
+        if (timeShown >= 2.0f)
+        {
+            success.text = "";
+            fail.text = "";
+        }
+
+        // show the shoulder width cue
+        GameObject.Find("Cue").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Cueprefab.SetActive(true);
+            Cueprefab.transform.localScale = new Vector3(inputF, 0.05f, 0.05f);
+        }
+        );
+        Cueprefab.transform.position = GameObject.Find("First Person Camera").transform.position + new Vector3(-1.0F, -1.0f, 0.5F);
+        Cueprefab.transform.rotation = GameObject.Find("First Person Camera").transform.rotation;
+    }
+}
---
+++ AJ20200213/Assets/PoleSpace.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoleSpace : MonoBehaviour
+{
+    public int TranslateSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 amountToMove = (GameObject.Find("PoleRight").transform.position - GameObject.Find("PoleLeft").transform.position).normalized;
+        // Vector3 amountToMove = new Vector3(1, 0, 0);
+        Vector3 left = GameObject.Find("PoleLeft").transform.position;
+        Vector3 right = GameObject.Find("PoleRight").transform.position;
+        GameObject.Find("Inward").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            //Put your logic here
+            
+            GameObject.Find("PoleLeft").transform.position = Vector3.Lerp(left, left + amountToMove/40, 1);
+            GameObject.Find("PoleRight").transform.position = Vector3.Lerp(right, right - amountToMove/40, 1);
+        }
+        );
+        GameObject.Find("Outward").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            GameObject.Find("PoleLeft").transform.position = Vector3.Lerp(left, left - amountToMove/40, 1);
+            GameObject.Find("PoleRight").transform.position = Vector3.Lerp(right, right + amountToMove/40, 1);
+        }
+        );
+    }
+
+    //private void OnGUI()
+    //{
+
+    //    if (GUI.Button(new Rect(Screen.width - 500, 200, 400, 150), "Inward", style))
+    //    {
+    //        //Time.DeltaTime
+    //        //https://answers.unity.com/questions/490687/when-to-use-timedeltatime.html
+    //        GameObject.Find("PoleLeft").transform.Translate(Vector3.right * Time.deltaTime * TranslateSpeed);
+    //        GameObject.Find("PoleRight").transform.Translate(Vector3.right * Time.deltaTime * (-TranslateSpeed));
+    //        //GameObject.Find("PoleLeft").transform.Translate(Vector3.right);
+    //        //GameObject.Find("PoleRight").transform.Translate(-Vector3.right);
+    //    }
+    //    if (GUI.Button(new Rect(100, 200, 400, 150), "Outward", style))
+    //    {
+
+    //        GameObject.Find("PoleLeft").transform.Translate(Vector3.right * Time.deltaTime * (-TranslateSpeed));
+    //        GameObject.Find("PoleRight").transform.Translate(Vector3.right * Time.deltaTime * TranslateSpeed);
+    //        //GameObject.Find("PoleLeft").transform.Translate(-Vector3.right);
+    //        //GameObject.Find("PoleRight").transform.Translate(Vector3.right);
+    //    }
+
+
+    //} 
+
+}
